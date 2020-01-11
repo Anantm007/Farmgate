d678a390d3401f8e7a4fa2865853deb0ef2f9da3
Anant Mathur <anant.mathur007@gmail.com>
2020-01-11 14:12:27 +0530

    Shops on landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import Logo from "../../images/logo.png";
 import Carousel from './Carousel';
+import ShopsCard from './ShopsCard'
 const Landing = () => {
     return (
         <Fragment>
             <Carousel />
             <h1 style={{textAlign:'center', marginTop: '2rem'}}>OUR SHOPS</h1>
+            <ShopsCard />
         </Fragment>
     )
 }
