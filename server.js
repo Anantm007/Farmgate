c699ad22e131c9176ef03d454abec8d326f55422
Anant Mathur <anant.mathur007@gmail.com>
2020-02-22 10:52:10 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const morgan = require('morgan');
 const cors = require("cors");
 const colors = require("colors");
 
+const path = require('path'); 
+
 // Mongoose 
 const mongoose = require('mongoose');
 
@@ -67,6 +69,19 @@ app.use('/api/items', require('./routes/item'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/order', require('./routes/order'));
 app.use('/api', require('./routes/braintree'));
+
+
+// Serve Static Assets in production
+if(process.env.NODE_ENV === 'production')
+{
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
 
 // Starting the server
 app.listen(process.env.PORT || 5000, ()=>{
