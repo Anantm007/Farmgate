864eb4469cfb18b8abe63b94ef22aacd06e5157c
Anant Mathur <anant.mathur007@gmail.com>
2020-03-15 13:15:36 +0530

    Production build

---
+++ client/build/precache-manifest.876f1c1c4f0b99b7c270af814c00b810.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71fe6a65f966c7c9cbde20ed7ae686cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669cb06735bda67d841a",
+    "url": "/static/css/main.c8ebd139.chunk.css"
+  },
+  {
+    "revision": "cb96fdb9c345adc5492d",
+    "url": "/static/js/2.78e65be5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.78e65be5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "669cb06735bda67d841a",
+    "url": "/static/js/main.06b6c652.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "ea02437f0d639d4f600f6db2010a84b1",
+    "url": "/static/media/Farmgate_Ag_Privacy_Policy.ea02437f.pdf"
+  },
+  {
+    "revision": "4e928fe4588c444a1bec7d52e05a96c9",
+    "url": "/static/media/TermsAndConditions.4e928fe4.pdf"
+  },
+  {
+    "revision": "50156c11623951ae9800704074d16f5c",
+    "url": "/static/media/about2.50156c11.jpg"
+  },
+  {
+    "revision": "21ec1cb3dfd980b67a7b435a0898cc75",
+    "url": "/static/media/about3.21ec1cb3.jpg"
+  },
+  {
+    "revision": "b87a21a9a06dad6cb48e532294a98ea1",
+    "url": "/static/media/carousel1.b87a21a9.jpg"
+  },
+  {
+    "revision": "656d87d860e1fa92f8f12856821240d3",
+    "url": "/static/media/carousel2.656d87d8.jpg"
+  },
+  {
+    "revision": "9a3846d9350c21765dc5d0ea18083587",
+    "url": "/static/media/carousel3.9a3846d9.jpg"
+  },
+  {
+    "revision": "7bfe0bce5333fd8b284dfa9a975b23a6",
+    "url": "/static/media/logo.7bfe0bce.png"
+  }
+]);
