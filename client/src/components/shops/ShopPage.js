f1b00149bf95a2ed14b262187d8322abe1742e19
Anant Mathur <anant.mathur007@gmail.com>
2020-07-20 13:22:09 +0530

    remove no certificates found from frontend

--- client/src/components/shops/ShopPage.js
+++ client/src/components/shops/ShopPage.js
@@ -59,7 +59,7 @@ const ShopPage = (props) => {
       getCertificates(shopId).then(data => {
           if(data.success === false)
           {         
-              setError(data.message);
+              // setError(data.message);
           }
 
           else
