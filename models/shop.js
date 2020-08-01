056291f09605c41eeb99387701e75a8f43bf27fd
Anant Mathur <anant.mathur007@gmail.com>
2020-08-01 15:21:48 +0530

    Add website for monika's shop

--- models/shop.js
+++ models/shop.js
@@ -1,85 +1,90 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ShopSchema = new mongoose.Schema({
-
+const ShopSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        trim: true
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
     },
 
     email: {
-        type: String,
-        required: [true, 'Please add a valid email'],
-        trim: true,
-        unique: true
+      type: String,
+      required: [true, "Please add a valid email"],
+      trim: true,
+      unique: true,
     },
 
     password: {
-        type: String,
-        required: [true, 'Please add a password'],
-        minlength: 6
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
     },
 
     address: {
-        type: String,
-        required: [true, 'Please add an address'],
+      type: String,
+      required: [true, "Please add an address"],
     },
 
     zipCode: {
-        type: Number,
-        required: [true, 'Please add a zip code'],
+      type: Number,
+      required: [true, "Please add a zip code"],
     },
 
     phoneNumber: {
-        type: Number,
-        unique: true,
-        required: [true, 'Please add a phone number'],
+      type: Number,
+      unique: true,
+      required: [true, "Please add a phone number"],
     },
 
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     image: {
-        data: Buffer,
-        contentType: String
+      data: Buffer,
+      contentType: String,
     },
 
     ABN: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    
+
     facebook: {
-        type: String
+      type: String,
     },
 
     instagram: {
-        type: String,
+      type: String,
     },
 
-    items: [{
+    website: {
+      type: String,
+    },
+
+    items: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Item'
-    }],
+        ref: "Item",
+      },
+    ],
 
     invoiceChargePercentage: {
-        type: Number,
-        default: 22.5
+      type: Number,
+      default: 22.5,
     },
 
     resetPasswordToken: {
-        type: String
+      type: String,
     },
 
     resetPasswordExpire: {
-        type: String
-    }    
-
-}, {timestamps: true}
-
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Shop', ShopSchema);
+module.exports = mongoose.model("Shop", ShopSchema);
