f0a1e53aef4717a4f6a8449dcc7794b0bc2098fb
root <root@farmgateserver>
2020-08-24 17:59:14 +0530

    backup commit

---
+++ client/build/precache-manifest.19d87728a365fc5c5159687ffe85d862.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d72aa6d55eb9c072620e1e919cb1d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8192514f635639381321",
+    "url": "/static/css/main.dea7ad03.chunk.css"
+  },
+  {
+    "revision": "ae89a649ed2f579fc1e5",
+    "url": "/static/js/2.9fbefed4.chunk.js"
+  },
+  {
+    "revision": "a51e1a732616a65fe21698cc04856fb1",
+    "url": "/static/js/2.9fbefed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8192514f635639381321",
+    "url": "/static/js/main.378c404a.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "ea02437f0d639d4f600f6db2010a84b1",
+    "url": "/static/media/Farmgate_Ag_Privacy_Policy.ea02437f.pdf"
+  },
+  {
+    "revision": "3ffae37beacaf06e2468602f6339e3b1",
+    "url": "/static/media/Farmgate_Sitemap.3ffae37b.xml"
+  },
+  {
+    "revision": "bfe4a399d6ae5c4609245b0d1b5fbb36",
+    "url": "/static/media/TermsAndConditions.bfe4a399.pdf"
+  },
+  {
+    "revision": "07a592229ce7be7b8ac9eb092fcc56be",
+    "url": "/static/media/about2.07a59222.jpg"
+  },
+  {
+    "revision": "4778b12118e2a3aae0b4279e8df8af71",
+    "url": "/static/media/about3.4778b121.jpg"
+  },
+  {
+    "revision": "93703c7920aa4eb6382285130587367b",
+    "url": "/static/media/carousel1.93703c79.jpg"
+  },
+  {
+    "revision": "b3520ee361613cd7c9e1d7917304e6eb",
+    "url": "/static/media/carousel2.b3520ee3.jpg"
+  },
+  {
+    "revision": "e90d735dace82f6970bd6f20448f8423",
+    "url": "/static/media/carousel3.e90d735d.jpg"
+  },
+  {
+    "revision": "d50233dac5adf7fc8b104b243dbf14ba",
+    "url": "/static/media/login.d50233da.jpg"
+  },
+  {
+    "revision": "7bfe0bce5333fd8b284dfa9a975b23a6",
+    "url": "/static/media/logo.7bfe0bce.png"
+  }
+]);
